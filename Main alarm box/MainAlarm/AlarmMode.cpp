07ee5cc220f54d975a34f913aa5cc84d4b128378
nicholas <kontakt@nicholaspfaff.ch>
2020-05-03 21:28:21 +0200

    Refactored AlarmMode

---
+++ Main alarm box/MainAlarm/AlarmMode.cpp
@@ -0,0 +1,418 @@
+#include "AlarmMode.h"
+
+AlarmMode::AlarmMode(ILogger *_logger, UserIO *_io, ClockMode *_clock, CommSystem *_commSystem, StorageSystem *_storageSystem)
+    : logger{_logger}, io(_io), clock{_clock}, commSystem{_commSystem}, storageSystem{_storageSystem}, IMode("Alarm mode")
+{
+
+    //retrieve alarms that were stored in EEPROM
+    currentAlarmQuantity = storageSystem->retrieveStoredAlarmQuantityEEPROM();
+    Alarm *storedAlarms = storageSystem->retrieveAlarmsEEPROM(currentAlarmQuantity);
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        alarms[i] = storedAlarms[i];
+    }
+    delete[] storedAlarms;
+}
+
+void AlarmMode::resetAll() override
+{
+    for (int i{}; i < maxAlarmQuantity; i++)
+    {
+        alarms[i] = {};
+    }
+    currentAlarmQuantity = 0;
+
+    commSystem->disableLight();
+
+    //does reseting for EEPROM
+    storageSystem->clearEEPROM();
+}
+
+byte AlarmMode::getNumberOfOptions() const
+{
+    return numberOfOptions;
+}
+
+int AlarmMode::getTimeToActivateLightMin() const
+{
+    return timeToActivateLightMin;
+}
+
+void AlarmMode::displayOptions()
+{
+    const unsigned long displayOptionsIntervalMS PROGMEM = 3000;
+    //display different alarm mode options
+    unsigned long currentAlarmMillis = millis();
+    if ((unsigned long)(currentAlarmMillis - previousAlarmMillis) >= displayOptionsIntervalMS)
+    {
+        io->clearScreen();
+        io->setCursor(0, 0);
+        io->print("Select one (#=Quit):");
+        for (int i{1}; i < 4 && currentDisplayedOption < numberOfOptions; i++)
+        {
+            String optionName = optionNames[currentDisplayedOption];
+            io->setCursor(0, i);
+            io->print(optionName);
+            currentDisplayedOption++;
+        }
+
+        //check if need to rollover to first option
+        if (currentDisplayedOption >= numberOfOptions)
+        {
+            currentDisplayedOption = 0;
+        }
+
+        previousAlarmMillis = currentAlarmMillis; //restart display option interval
+    }
+}
+
+byte AlarmMode::selectOption()
+{
+    byte option = io->selectOption(numberOfOptions);
+    if (option)
+    {
+        currentDisplayedOption = 0; //make sure to start with displaying first option when displayOptions() is entered again
+    }
+    return option;
+}
+
+void AlarmMode::executeOption(int selectedOption)
+{
+    switch (selectedOption)
+    {
+    case 1:                      //display existing alarms
+        previousAlarmMillis = 0; //eliminate any possible delay
+        displayExistingAlarms();
+        break;
+    case 2: //create new alarm
+        createNewAlarm();
+        break;
+    case 3: //delete existing alarm
+        deleteExistingAlarm();
+        break;
+    case 4: //enable existing alarm
+        changeExistingAlarmStatus(true);
+        break;
+    case 5: //disable existing alarm
+        changeExistingAlarmStatus(false);
+        break;
+    case 6: //delete all existing alarms
+        deleteAllExistingAlarms();
+        break;
+    default:
+        logger->logError(F("Tried to execute non-existing alarm option"), F("AlarmMode, executeOption"));
+        break;
+    }
+    previousAlarmMillis = 0; //eliminate any possible delay
+
+    //store any updates made to alarms => can restore them after power outage
+    storageSystem->updateAlarmsEEPROM(alarms, currentAlarmQuantity);
+}
+
+void AlarmMode::displayExistingAlarms()
+{
+    const unsigned long displayAlarmsIntervalMS PROGMEM = 3000;
+
+    //continue alarm display until # is entered (quit)
+    while (!io->enteredHash())
+    {
+        unsigned long currentAlarmMillis = millis();
+        if ((unsigned long)(currentAlarmMillis - previousAlarmMillis) >= displayAlarmsIntervalMS)
+        {
+            io->clearScreen();
+            io->setCursor(0, 0);
+            io->print(F("Alarms (#=Quit):"));
+
+            if (currentAlarmQuantity == 0)
+            {
+                io->setCursor(0, 2);
+                io->print(F("0 alarms to display"));
+            }
+            else
+            {
+                Alarm alarm = alarms[currentDisplayedAlarm];
+                io->setCursor(0, 1);
+                io->print(F("Alarm "));
+                io->print(currentDisplayedAlarm + 1); //first alarm has number 1 and is stored in alarms[0]
+                io->setCursor(0, 2);
+                io->print(F("Time: "));
+                io->printDigits(alarm.getHour(), true);
+                io->printDigits(alarm.getMinute());
+                io->setCursor(0, 3);
+                io->print(F("Status: "));
+                io->print(alarm.getStatus() ? F("Enabled") : F("Disabled"));
+
+                currentDisplayedAlarm++;
+
+                //check if need to rollover to first option
+                if (currentDisplayedAlarm >= currentAlarmQuantity)
+                {
+                    currentDisplayedAlarm = 0;
+                }
+            }
+
+            previousAlarmMillis = currentAlarmMillis; //restart display option interval
+        }
+    }
+    currentDisplayedAlarm = 0; //make sure to start with displaying first alarm when displayExistingAlarms is entered again
+}
+
+void AlarmMode::createNewAlarm()
+{
+    io->clearScreen();
+
+    if (currentAlarmQuantity >= maxAlarmQuantity)
+    {
+        while (!io->enteredHash())
+        {
+            io->setCursor(0, 0);
+            io->print(F("Max alarm quantity"));
+            io->setCursor(0, 1);
+            io->print(F("reached"));
+            io->setCursor(0, 3);
+            io->print(F("Enter # to quit"));
+        }
+    }
+    else
+    {
+        io->setCursor(0, 0);
+        io->print(F("Entering alarm time"));
+        int *alarmTime = io->getTime();
+        if (!alarmTime)
+        {
+            return;
+        }
+
+        //intentionally using a narrowing conversion
+        byte hour{alarmTime[0]}, minute{alarmTime[1]};
+        delete[] alarmTime;
+
+        Alarm newAlarm(hour, minute, true);
+        alarms[currentAlarmQuantity] = newAlarm;
+        currentAlarmQuantity++;
+    }
+}
+
+void AlarmMode::deleteExistingAlarm()
+{
+    io->clearScreen();
+
+    if (currentAlarmQuantity == 0)
+    {
+        while (!io->enteredHash())
+        {
+            io->setCursor(0, 0);
+            io->print(F("No alarm to display"));
+            io->setCursor(0, 2);
+            io->print(F("Enter # to quit"));
+        }
+    }
+    else
+    {
+        int alarmNumber{}; //alarm index in alarms[] will be alarmNumber - 1
+
+        io->setCursor(0, 3);
+        io->print(F("Enter # to quit"));
+        io->setCursor(0, 0);
+        io->print(F("Enter number of"));
+        io->setCursor(0, 1);
+        io->print(F("alarm to delete: "));
+        io->showCursor();
+        char input = io->getValidDigitOrHashBlocking();
+
+        if (input == '#')
+        {
+            return;
+        }
+        else if (input && (int)input - 48 <= currentAlarmQuantity)
+        {
+            io->print(input);
+            io->hideCursor();
+            delay(500); //for user to see their input
+
+            int alarmIdx{(int)input - 49}; //alarm index in alarms[] (alarm number - 1)
+
+            //disable light that was enabled due to an alarm ringing soon
+            if (checkIfActivatedLightByAlarm(alarms[alarmIdx]))
+            {
+                commSystem->disableLight();
+            }
+
+            if (alarmNumber == currentAlarmQuantity - 1)
+            {
+                //remove last element from array
+                alarms[alarmIdx] = {};
+            }
+            else
+            {
+                //removing element from middle of array and hence need to make sure that existing alarms fill first spaces of array and not existing alarms are at end of array
+                Alarm remainingAlarms[maxAlarmQuantity]{};
+                int j{};
+                for (int i{}; i < maxAlarmQuantity; i++)
+                {
+                    if (i != alarmIdx)
+                    {
+                        remainingAlarms[j] = alarms[i];
+                        j++;
+                    }
+                }
+                memcpy(alarms, remainingAlarms, sizeof(alarms));
+            }
+            currentAlarmQuantity--;
+        }
+        else
+        {
+            //restart user input
+            deleteExistingAlarm();
+        }
+    }
+}
+
+void AlarmMode::changeExistingAlarmStatus(bool newStatus)
+{
+    io->clearScreen();
+
+    if (currentAlarmQuantity == 0)
+    {
+        while (!io->enteredHash())
+        {
+            io->setCursor(0, 0);
+            if (newStatus)
+            {
+                io->print(F("No alarm to enable"));
+            }
+            else
+            {
+                io->print(F("No alarm to disable"));
+            }
+            io->setCursor(0, 2);
+            io->print(F("Enter # to quit"));
+        }
+    }
+    else
+    {
+        int alarmNumber{}; //alarm index in alarms[] will be alarmNumber - 1
+
+        io->setCursor(0, 3);
+        io->print(F("Enter # to quit"));
+        io->setCursor(0, 0);
+        io->print(F("Enter number of"));
+        io->setCursor(0, 1);
+        if (newStatus)
+        {
+            io->print(F("alarm to enable: "));
+        }
+        else
+        {
+            io->print(F("alarm to disable: "));
+        }
+        io->showCursor();
+        char input = io->getValidDigitOrHashBlocking();
+
+        if (input == '#')
+        {
+            return;
+        }
+        else if (input && (int)input - 48 <= currentAlarmQuantity)
+        {
+            io->print(input);
+            io->hideCursor();
+            delay(500); //for user to see their input
+
+            int alarmIdx{(int)input - 49}; //alarm index in alarms[] (alarm number - 1)
+            alarms[alarmIdx].setStatus(newStatus);
+        }
+        else
+        {
+            //restart user input
+            deleteExistingAlarm();
+        }
+    }
+}
+
+void AlarmMode::deleteAllExistingAlarms()
+{
+    io->clearScreen();
+
+    //disable light that was enabled due to an alarm ringing soon
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        if (checkIfActivatedLightByAlarm(alarms[i]))
+        {
+            commSystem->disableLight();
+        }
+    }
+
+    memset(alarms, 0, sizeof(alarms));
+    currentAlarmQuantity = 0;
+
+    io->setCursor(0, 1);
+    io->print(F("Deleted all"));
+    io->setCursor(0, 2);
+    io->print(F("existing alarms"));
+    delay(1300);
+}
+
+void AlarmMode::changeExistingAlarmStatusOnId(int id, bool newStatus)
+{
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        if (alarms[i].getId() == id)
+        {
+            alarms[i].setStatus(newStatus);
+        }
+    }
+}
+
+bool AlarmMode::isAnyAlarmEnabled() const
+{
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        if (alarms[i].getStatus())
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int AlarmMode::getActiveAlarmId() const
+{
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        if (alarms[i].getStatus() && alarms[i].getHour() == clock->getHour() && alarms[i].getMinute() == clock->getMinute())
+        {
+            return alarms[i].getId();
+        }
+    }
+    return 0;
+}
+
+bool AlarmMode::activateLight() const
+{
+    for (int i{}; i < currentAlarmQuantity; i++)
+    {
+        if (alarms[i].getStatus() && alarms[i].getMinute() >= timeToActivateLightMin && alarms[i].getHour() == clock->getHour() && alarms[i].getMinute() - timeToActivateLightMin == clock->getMinute())
+        {
+            return true;
+        }
+        else if (alarms[i].getStatus() && (alarms[i].getHour() - 1) == clock->getHour() && (alarms[i].getMinute() + 60 - timeToActivateLightMin) == clock->getMinute())
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool AlarmMode::checkIfActivatedLightByAlarm(const Alarm &alarm) const
+{
+    if (alarm.getStatus() && alarm.getMinute() >= timeToActivateLightMin && alarm.getHour() == clock->getHour() && alarm.getMinute() - timeToActivateLightMin <= clock->getMinute())
+    {
+        return true;
+    }
+    else if (alarm.getStatus() && (alarm.getHour() - 1) == clock->getHour() && (alarm.getMinute() + 60 - timeToActivateLightMin) <= clock->getMinute())
+    {
+        return true;
+    }
+    return false;
+}
--- Main alarm box/MainAlarm/AlarmMode.h
+++ Main alarm box/MainAlarm/AlarmMode.h
@@ -12,390 +12,75 @@
 
 class AlarmMode : public IMode
 {
-  private:
-    const static byte maxAlarmQuantity PROGMEM = 3;
-    const static byte timeToActivateLightMin PROGMEM = 1; //change to larger value after testing!
+private:
+  const static byte maxAlarmQuantity PROGMEM = 3;
+  const static byte timeToActivateLightMin PROGMEM = 20; //change based on testing experience
 
-    int currentAlarmQuantity{};
-    int currentDisplayedAlarm{};
-    Alarm alarms[maxAlarmQuantity] {}; //position 0 in array holds alarm with number 1
+  int currentAlarmQuantity{};
+  int currentDisplayedAlarm{};
+  Alarm alarms[maxAlarmQuantity]{}; //position 0 in array holds alarm with number 1
 
-    unsigned long previousAlarmMillis;
+  unsigned long previousAlarmMillis;
 
-    const static byte numberOfOptions PROGMEM = 6;
-    const String optionNames[numberOfOptions] = {"1. Display alarms", "2. New alarm", "3. Delete alarm", "4. Enable alarm", "5. Disable alarm", "6. Delete all alarms"};
-    byte currentDisplayedOption{};
+  const static byte numberOfOptions PROGMEM = 6;
+  const String optionNames[numberOfOptions] = {"1. Display alarms", "2. New alarm", "3. Delete alarm", "4. Enable alarm", "5. Disable alarm", "6. Delete all alarms"};
+  byte currentDisplayedOption{};
 
-    ILogger *logger;
-    UserIO *io;
-    ClockMode *clock;
-    CommSystem *commSystem;
-    StorageSystem *storageSystem;
+  ILogger *logger;
+  UserIO *io;
+  ClockMode *clock;
+  CommSystem *commSystem;
+  StorageSystem *storageSystem;
 
-  public:
-    AlarmMode(ILogger *_logger, UserIO *_io, ClockMode *_clock, CommSystem *_commSystem, StorageSystem *_storageSystem)
-      : logger{_logger}, io(_io), clock{_clock}, commSystem{_commSystem}, storageSystem{_storageSystem}, IMode("Alarm mode") {
+public:
+  AlarmMode(ILogger *_logger, UserIO *_io, ClockMode *_clock, CommSystem *_commSystem, StorageSystem *_storageSystem);
 
-      //retrieve alarms that were stored in EEPROM
-      currentAlarmQuantity = storageSystem->retrieveStoredAlarmQuantityEEPROM();
-      Alarm* storedAlarms = storageSystem->retrieveAlarmsEEPROM(currentAlarmQuantity);
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        alarms[i] = storedAlarms[i];
-      }
-      delete[] storedAlarms;
-    }
+  void resetAll() override;
 
-    void resetAll() override
-    {
-      for (int i{}; i < maxAlarmQuantity; i++)
-      {
-        alarms[i] = {};
-      }
-      currentAlarmQuantity = 0;
+  byte getNumberOfOptions() const;
 
-      commSystem->disableLight();
+  int getTimeToActivateLightMin() const;
 
-      //does reseting for EEPROM
-      storageSystem->clearEEPROM();
-    }
+  //displays all available options
+  void displayOptions();
 
-    byte getNumberOfOptions() const {
-      return numberOfOptions;
-    }
+  //uses UserIO to get the selected option form the user input
+  byte selectOption();
 
-    int getTimeToActivateLightMin() const {
-      return timeToActivateLightMin;
-    }
+  //executeds an option based on the option's number
+  void executeOption(int selectedOption);
 
-    void displayOptions() {
-      const unsigned long displayOptionsIntervalMS PROGMEM = 3000;
-      //display different alarm mode options
-      unsigned long currentAlarmMillis = millis();
-      if ((unsigned long)(currentAlarmMillis - previousAlarmMillis) >= displayOptionsIntervalMS) {
-        io->clearScreen();
-        io->setCursor(0, 0);
-        io->print("Select one (#=Quit):");
-        for (int i{1}; i < 4 && currentDisplayedOption < numberOfOptions; i++) {
-          String optionName = optionNames[currentDisplayedOption];
-          io->setCursor(0, i);
-          io->print(optionName);
-          currentDisplayedOption++;
-        }
+  //option 1
+  void displayExistingAlarms();
 
-        //check if need to rollover to first option
-        if (currentDisplayedOption >= numberOfOptions) {
-          currentDisplayedOption = 0;
-        }
+  //option 2
+  void createNewAlarm();
 
-        previousAlarmMillis = currentAlarmMillis; //restart display option interval
-      }
-    }
+  //option 3
+  void deleteExistingAlarm();
 
-    byte selectOption() {
-      byte option = io->selectOption(numberOfOptions);
-      if (option) {
-        currentDisplayedOption = 0; //make sure to start with displaying first option when displayOptions() is entered again
-      }
-      return option;
-    }
+  //option 4 and 5 (true to enable, false to disable)
+  void changeExistingAlarmStatus(bool newStatus);
 
-    //executes selected option
-    //blocking operation
-    void executeOption(int selectedOption) {
-      switch (selectedOption) {
-        case 1: //display existing alarms
-          previousAlarmMillis = 0; //eliminate any possible delay
-          displayExistingAlarms();
-          break;
-        case 2: //create new alarm
-          createNewAlarm();
-          break;
-        case 3: //delete existing alarm
-          deleteExistingAlarm();
-          break;
-        case 4: //enable existing alarm
-          changeExistingAlarmStatus(true);
-          break;
-        case 5: //disable existing alarm
-          changeExistingAlarmStatus(false);
-          break;
-        case 6: //delete all existing alarms
-          deleteAllExistingAlarms();
-          break;
-        default:
-          logger->logError(F("Tried to execute non-existing alarm option"), F("AlarmMode, executeOption"));
-          break;
-      }
-      previousAlarmMillis = 0; //eliminate any possible delay
+  //option 6
+  void deleteAllExistingAlarms();
 
-      //store any updates made to alarms => can restore them after power outage
-      storageSystem->updateAlarmsEEPROM(alarms, currentAlarmQuantity);
-    }
+  //true to enable, false to disable
+  void changeExistingAlarmStatusOnId(int id, bool newStatus);
 
-    //option 1
-    void displayExistingAlarms()
-    {
-      const unsigned long displayAlarmsIntervalMS PROGMEM = 3000;
+  //checks if any alarm is enabled
+  bool isAnyAlarmEnabled() const;
 
-      //continue alarm display until # is entered (quit)
-      while (!io->enteredHash()) {
-        unsigned long currentAlarmMillis = millis();
-        if ((unsigned long)(currentAlarmMillis - previousAlarmMillis) >= displayAlarmsIntervalMS) {
-          io->clearScreen();
-          io->setCursor(0, 0);
-          io->print(F("Alarms (#=Quit):"));
+  //checks whether it is time for an alarm to ring by comparing alarm time with current time
+  //returns id of active alarm, or 0 if no active alarm exists
+  int getActiveAlarmId() const;
 
-          if (currentAlarmQuantity == 0) {
-            io->setCursor(0, 2);
-            io->print(F("0 alarms to display"));
-          } else {
-            Alarm alarm = alarms[currentDisplayedAlarm];
-            io->setCursor(0, 1);
-            io->print(F("Alarm "));
-            io->print(currentDisplayedAlarm + 1); //first alarm has number 1 and is stored in alarms[0]
-            io->setCursor(0, 2);
-            io->print(F("Time: "));
-            io->printDigits(alarm.getHour(), true);
-            io->printDigits(alarm.getMinute());
-            io->setCursor(0, 3);
-            io->print(F("Status: "));
-            io->print(alarm.getStatus() ? F("Enabled") : F("Disabled"));
+  //returns true if should activate the light (alarm is going to ring soon as defined by set time)
+  bool activateLight() const;
 
-            currentDisplayedAlarm++;
-
-            //check if need to rollover to first option
-            if (currentDisplayedAlarm >= currentAlarmQuantity) {
-              currentDisplayedAlarm = 0;
-            }
-          }
-
-          previousAlarmMillis = currentAlarmMillis; //restart display option interval
-        }
-      }
-      currentDisplayedAlarm = 0; //make sure to start with displaying first alarm when displayExistingAlarms is entered again
-    }
-
-    //option 2
-    void createNewAlarm()
-    {
-      io->clearScreen();
-
-      if (currentAlarmQuantity >= maxAlarmQuantity)
-      {
-        while (!io->enteredHash()) {
-          io->setCursor(0, 0);
-          io->print(F("Max alarm quantity"));
-          io->setCursor(0, 1);
-          io->print(F("reached"));
-          io->setCursor(0, 3);
-          io->print(F("Enter # to quit"));
-        }
-      } else {
-        io->setCursor(0, 0);
-        io->print(F("Entering alarm time"));
-        int* alarmTime = io->getTime();
-        if (!alarmTime) {
-          return;
-        }
-
-        //intentionally using a narrowing conversion
-        byte hour{alarmTime[0]}, minute{alarmTime[1]};
-        delete[] alarmTime;
-
-        Alarm newAlarm(hour, minute, true);
-        alarms[currentAlarmQuantity] = newAlarm;
-        currentAlarmQuantity++;
-      }
-    }
-
-    //option 3
-    void deleteExistingAlarm()
-    {
-      io->clearScreen();
-
-      if (currentAlarmQuantity == 0)
-      {
-        while (!io->enteredHash()) {
-          io->setCursor(0, 0);
-          io->print(F("No alarm to display"));
-          io->setCursor(0, 2);
-          io->print(F("Enter # to quit"));
-        }
-      } else {
-        int alarmNumber{}; //alarm index in alarms[] will be alarmNumber - 1
-
-        io->setCursor(0, 3);
-        io->print(F("Enter # to quit"));
-        io->setCursor(0, 0);
-        io->print(F("Enter number of"));
-        io->setCursor(0, 1);
-        io->print(F("alarm to delete: "));
-        io->showCursor();
-        char input = io->getValidDigitOrHashBlocking();
-
-        if (input == '#') {
-          return;
-        } else if (input && (int)input - 48 <= currentAlarmQuantity) {
-          io->print(input);
-          io->hideCursor();
-          delay(500); //for user to see their input
-
-          int alarmIdx{(int)input - 49}; //alarm index in alarms[] (alarm number - 1)
-
-          //disable light that was enabled due to an alarm ringing soon
-          if (checkIfActivatedLightByAlarm(alarms[alarmIdx])) {
-            commSystem->disableLight();
-          }
-
-          if (alarmNumber == currentAlarmQuantity - 1) {
-            //remove last element from array
-            alarms[alarmIdx] = {};
-          } else {
-            //removing element from middle of array and hence need to make sure that existing alarms fill first spaces of array and not existing alarms are at end of array
-            Alarm remainingAlarms[maxAlarmQuantity] {};
-            int j{};
-            for (int i{}; i < maxAlarmQuantity; i++) {
-              if (i != alarmIdx) {
-                remainingAlarms[j] = alarms[i];
-                j++;
-              }
-            }
-            memcpy(alarms, remainingAlarms, sizeof(alarms));
-          }
-          currentAlarmQuantity--;
-
-        } else {
-          //restart user input
-          deleteExistingAlarm();
-        }
-      }
-    }
-
-    //option 4 and 5 (true to enable, false to disable)
-    void changeExistingAlarmStatus(bool newStatus)
-    {
-      io->clearScreen();
-
-      if (currentAlarmQuantity == 0)
-      {
-        while (!io->enteredHash()) {
-          io->setCursor(0, 0);
-          if (newStatus) {
-            io->print(F("No alarm to enable"));
-          } else {
-            io->print(F("No alarm to disable"));
-          }
-          io->setCursor(0, 2);
-          io->print(F("Enter # to quit"));
-        }
-      } else {
-        int alarmNumber{}; //alarm index in alarms[] will be alarmNumber - 1
-
-        io->setCursor(0, 3);
-        io->print(F("Enter # to quit"));
-        io->setCursor(0, 0);
-        io->print(F("Enter number of"));
-        io->setCursor(0, 1);
-        if (newStatus) {
-          io->print(F("alarm to enable: "));
-        } else {
-          io->print(F("alarm to disable: "));
-        }
-        io->showCursor();
-        char input = io->getValidDigitOrHashBlocking();
-
-        if (input == '#') {
-          return;
-        } else if (input && (int)input - 48 <= currentAlarmQuantity) {
-          io->print(input);
-          io->hideCursor();
-          delay(500); //for user to see their input
-
-          int alarmIdx{(int)input - 49}; //alarm index in alarms[] (alarm number - 1)
-          alarms[alarmIdx].setStatus(newStatus);
-        } else {
-          //restart user input
-          deleteExistingAlarm();
-        }
-      }
-    }
-
-    //option 6
-    void deleteAllExistingAlarms() {
-      io->clearScreen();
-
-      //disable light that was enabled due to an alarm ringing soon
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        if (checkIfActivatedLightByAlarm(alarms[i])) {
-          commSystem->disableLight();
-        }
-      }
-
-      memset(alarms, 0, sizeof(alarms));
-      currentAlarmQuantity = 0;
-
-      io->setCursor(0, 1);
-      io->print(F("Deleted all"));
-      io->setCursor(0, 2);
-      io->print(F("existing alarms"));
-      delay(1300);
-    }
-
-    void changeExistingAlarmStatusOnId(int id, bool newStatus) {
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        if (alarms[i].getId() == id) {
-          alarms[i].setStatus(newStatus);
-        }
-      }
-    }
-
-    //checks if any alarm is enabled
-    bool isAnyAlarmEnabled() const {
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        if (alarms[i].getStatus())
-        {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    //checks whether it is time for an alarm to ring by comparing alarm time with current time
-    //returns id of active alarm or 0 if no active alarm
-    int getActiveAlarmId() const {
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        if (alarms[i].getStatus() && alarms[i].getHour() == clock->getHour() && alarms[i].getMinute() == clock->getMinute())
-        {
-          return alarms[i].getId();
-        }
-      }
-      return 0;
-    }
-
-    //start activating light set time before alarm starts
-    bool activateLight() const {
-      for (int i{}; i < currentAlarmQuantity; i++) {
-        if (alarms[i].getStatus() && alarms[i].getMinute() >= timeToActivateLightMin && alarms[i].getHour() == clock->getHour() && alarms[i].getMinute() - timeToActivateLightMin == clock->getMinute()) {
-          return true;
-        }
-        else if (alarms[i].getStatus() && (alarms[i].getHour() - 1) == clock->getHour() && (alarms[i].getMinute() + 60 - timeToActivateLightMin) == clock->getMinute())
-        {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    bool checkIfActivatedLightByAlarm(const Alarm & alarm) const {
-      if (alarm.getStatus() && alarm.getMinute() >= timeToActivateLightMin && alarm.getHour() == clock->getHour() && alarm.getMinute() - timeToActivateLightMin <= clock->getMinute()) {
-        return true;
-      }
-      else if (alarm.getStatus() && (alarm.getHour() - 1) == clock->getHour() && (alarm.getMinute() + 60 - timeToActivateLightMin) <= clock->getMinute())
-      {
-        return true;
-      }
-      return false;
-    }
+  //returns true if the light has been activated due to an alarm ringing soon
+  //this is usefull for knowing if we might have to turn of the light again
+  bool checkIfActivatedLightByAlarm(const Alarm &alarm) const;
 };
 
 #endif
