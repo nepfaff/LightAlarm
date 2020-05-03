9384b991be497a9ff46bdf2156e241e69c158ac8
nicholas <kontakt@nicholaspfaff.ch>
2020-05-03 21:11:29 +0200

    Refactored Alarm

---
+++ Main alarm box/MainAlarm/Alarm.cpp
@@ -0,0 +1,40 @@
+#include "Alarm.h"
+
+Alarm::Alarm()
+    : id{255}, hour{255}, minute{255}, isEnabled{false} {}
+
+Alarm::Alarm(byte _hour, byte _minute, bool _isEnabled)
+    : id{currentId}, hour{_hour}, minute{_minute}, isEnabled{_isEnabled}
+{
+    currentId++;
+
+    //make sure that currentId is never zero (overflow)
+    if (currentId == 0)
+    {
+        currentId++;
+    }
+}
+
+byte Alarm::getId() const
+{
+    return id;
+}
+byte Alarm::getHour() const
+{
+    return hour;
+}
+byte Alarm::getMinute() const
+{
+    return minute;
+}
+bool Alarm::getStatus() const
+{
+    return isEnabled;
+}
+
+int Alarm::setStatus(bool newStatus)
+{
+    isEnabled = newStatus;
+}
+
+byte Alarm::currentId = 1;
--- Main alarm box/MainAlarm/Alarm.h
+++ Main alarm box/MainAlarm/Alarm.h
@@ -5,52 +5,27 @@
 
 class Alarm
 {
-  private:
-    static byte currentId; //id needs to start with 1 as 0 is used for returning false
+private:
+  //id needs to start with 1 as 0 is used for returning false
+  static byte currentId;
 
-    byte id;
-    byte hour;
-    byte minute;
-    bool isEnabled;
-    //could pottetially add soundrack name to select from sd card
+  byte id;
+  byte hour;
+  byte minute;
+  bool isEnabled;
+  //could pottetially add soundrack name to select from sd card
 
-  public:
-    Alarm()
-      : id{255}, hour{255}, minute{255}, isEnabled{false} {}
-    Alarm(byte _hour, byte _minute, bool _isEnabled)
-      : id{currentId}, hour{_hour}, minute{_minute}, isEnabled{_isEnabled} {
-      currentId++;
+public:
+  Alarm();
+  Alarm(byte _hour, byte _minute, bool _isEnabled);
 
-      //make sure that currentId is never zero (overflow)
-      if(currentId == 0){
-        currentId++;
-      }
-    }
+  //only provide getters => Cannot modify alarm time after creation (needs to be destoyed and recreated)
+  byte getId() const;
+  byte getHour() const;
+  byte getMinute() const;
+  bool getStatus() const;
 
-    //only provide getters => Cannot modify alarm time after creation (needs to be destoyed and recreated)
-    byte getId() const
-    {
-      return id;
-    }
-    byte getHour() const
-    {
-      return hour;
-    }
-    byte getMinute() const
-    {
-      return minute;
-    }
-    bool getStatus() const
-    {
-      return isEnabled;
-    }
-    
-    int setStatus(bool newStatus)
-    {
-      isEnabled = newStatus;
-    }
+  int setStatus(bool newStatus);
 };
-
-byte Alarm::currentId = 1;
 
 #endif
