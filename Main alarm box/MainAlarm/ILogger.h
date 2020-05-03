754617a8417631fbd11e5e754b51668c185d7db6
nicholas <kontakt@nicholaspfaff.ch>
2020-05-03 21:45:38 +0200

    Added description to ILogger.h

--- Main alarm box/MainAlarm/ILogger.h
+++ Main alarm box/MainAlarm/ILogger.h
@@ -3,14 +3,17 @@
 
 #include "Arduino.h"
 
+/*
+  Interface that all loggers should be derived from
+*/
 class ILogger
 {
-  public:
-    virtual ~ILogger(){};
-    
-    //error location is a combination of class and function
-    virtual void logError(String msg, String errorLocation) const = 0;
-    virtual void logInfo(String msg) const = 0;
+public:
+  virtual ~ILogger(){};
+
+  //error location is a combination of class and function
+  virtual void logError(String msg, String errorLocation) const = 0;
+  virtual void logInfo(String msg) const = 0;
 };
 
 #endif
